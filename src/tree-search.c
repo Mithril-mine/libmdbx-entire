39395cb4a43b93da313916c3563ea375314d3fef
Леонид Юрьев (Leonid Yuriev) <leo@yuriev.ru>
2026-04-16 01:11:45 +0300

    mdbx: fix/refine cursor_to_search_branch/foliage().

--- src/tree-search.c
+++ src/tree-search.c
@@ -402,8 +402,13 @@ cursor_to_search_foliage(const MDBX_cursor *mc) {
       return search_foliage_lenfast_dupfix;
 
     if (mc->tree->flags & MDBX_INTEGERKEY) {
-      size_t ordinal = 0, keylen = mc->tree->dupfix_size;
-      cASSERT0(mc, mc->clc->k.lmax == mc->clc->k.lmin && mc->clc->k.lmax == keylen && (keylen == 4 || keylen == 8));
+      const size_t keylen = mc->tree->dupfix_size;
+      cASSERT0(mc, keylen >= mc->clc->k.lmin && keylen <= mc->clc->k.lmax && (keylen == 4 || keylen == 8));
+      if (/* paranoia */ keylen >= mc->clc->k.lmin && keylen <= mc->clc->k.lmax && (keylen == 4 || keylen == 8)) {
+        mc->clc->k.lmin = keylen;
+        mc->clc->k.lmax = keylen;
+      }
+      size_t ordinal = 0;
 #ifndef cmp_uint_align2
       if (comparator == cmp_uint_align2)
         ordinal = keylen;
@@ -415,7 +420,7 @@ cursor_to_search_foliage(const MDBX_cursor *mc) {
       if (comparator == cmp_uint_unaligned)
         ordinal = keylen;
       if (ordinal) {
-        if ((mc->txn->env->flags & MDBX_VALIDATION) == 0) {
+        if ((mc->txn->env->flags & MDBX_VALIDATION) == 0 && ordinal == mc->clc->k.lmin && ordinal == mc->clc->k.lmax) {
           if (ordinal == 4)
             return search_foliage_uint32_dupfix;
           if (ordinal == 8)
@@ -436,8 +441,16 @@ cursor_to_search_foliage(const MDBX_cursor *mc) {
     return search_foliage_lenfast_usual;
 
   if (mc->tree->flags & MDBX_INTEGERKEY) {
-    size_t ordinal = 0, keylen = mc->clc->k.lmax;
-    cASSERT0(mc, mc->clc->k.lmax == mc->clc->k.lmin && (keylen == 4 || keylen == 8));
+    cASSERT0(mc, mc->top >= 0);
+    page_t *mp = mc->pg[mc->top];
+    cASSERT0(mc, !is_dupfix_leaf(mp) && page_numkeys(mp) > 0);
+    const size_t keylen = node_ks(page_node(mp, 0));
+    cASSERT0(mc, keylen >= mc->clc->k.lmin && keylen <= mc->clc->k.lmax && (keylen == 4 || keylen == 8));
+    if (/* paranoia */ keylen >= mc->clc->k.lmin && keylen <= mc->clc->k.lmax && (keylen == 4 || keylen == 8)) {
+      mc->clc->k.lmin = keylen;
+      mc->clc->k.lmax = keylen;
+    }
+    size_t ordinal = 0;
 #ifndef cmp_uint_align2
     if (comparator == cmp_uint_align2)
       ordinal = keylen;
@@ -449,7 +462,7 @@ cursor_to_search_foliage(const MDBX_cursor *mc) {
     if (comparator == cmp_uint_unaligned)
       ordinal = keylen;
     if (ordinal) {
-      if ((mc->txn->env->flags & MDBX_VALIDATION) == 0 && ordinal == mc->clc->k.lmin) {
+      if ((mc->txn->env->flags & MDBX_VALIDATION) == 0 && ordinal == mc->clc->k.lmin && ordinal == mc->clc->k.lmax) {
         if (ordinal == 4)
           return search_foliage_uint32_usual;
         if (ordinal == 8)
@@ -546,7 +559,17 @@ cursor_to_search_branch(const MDBX_cursor *mc) {
     return search_branch_lenfast;
 
   if (mc->tree->flags & MDBX_INTEGERKEY) {
-    size_t ordinal = 0, keylen = mc->clc->k.lmax;
+    cASSERT0(mc, mc->top >= 0);
+    page_t *mp = mc->pg[mc->top];
+    cASSERT0(mc, page_numkeys(mp) > 0);
+    STATIC_ASSERT(P_BRANCH == 1);
+    const size_t keylen = is_dupfix_leaf(mp) ? mp->dupfix_ksize : node_ks(page_node(mp, mp->flags & P_BRANCH));
+    cASSERT0(mc, keylen >= mc->clc->k.lmin && keylen <= mc->clc->k.lmax && (keylen == 4 || keylen == 8));
+    if (/* paranoia */ keylen >= mc->clc->k.lmin && keylen <= mc->clc->k.lmax && (keylen == 4 || keylen == 8)) {
+      mc->clc->k.lmin = keylen;
+      mc->clc->k.lmax = keylen;
+    }
+    size_t ordinal = 0;
 #ifndef cmp_uint_align2
     if (comparator == cmp_uint_align2)
       ordinal = keylen;
@@ -558,7 +581,7 @@ cursor_to_search_branch(const MDBX_cursor *mc) {
     if (comparator == cmp_uint_unaligned)
       ordinal = keylen;
     if (ordinal) {
-      if ((mc->txn->env->flags & MDBX_VALIDATION) == 0 && ordinal == mc->clc->k.lmin) {
+      if ((mc->txn->env->flags & MDBX_VALIDATION) == 0 && ordinal == mc->clc->k.lmin && ordinal == mc->clc->k.lmax) {
         if (ordinal == 4)
           return search_branch_uint32;
         if (ordinal == 8)
